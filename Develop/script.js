5a3e6bbb47376db0dbf7686ac4c0c06ed790c6b0
Jose Plasencia <josejpd3@gmail.com>
2022-10-02 13:19:36 -0400

    added generated password to page, comments, and fixed bugs

--- Develop/script.js
+++ Develop/script.js
@@ -3,19 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
 
 function generatePassword () {
-  let chars = ""
+  let chars = "abcdefghijklmnopqrstuvwxyz"
+  let newpassword = "";
 
+// Prompts
   let minimum = window.prompt("Minimum Password Length: ")
   let max = window.prompt("Maximum Password Length: ")
-  let lower = window.prompt("Inlclude Lowercase? \n (Y) or (N)")
-  let upper = window.prompt("Inlclude Uppercase? \n (Y) or (N)")
-  let num = window.prompt("Inlclude Numbers? \n (Y) or (N)")
-  let special = window.prompt("Inlclude Special Characters? \n (Y) or (N)")
+  let lower = window.prompt("Inlclude Lowercase? \n (Y) for yes")
+  let upper = window.prompt("Inlclude Uppercase? \n (Y) for yes")
+  let num = window.prompt("Inlclude Numbers? \n (Y) for yes")
+  let special = window.prompt("Inlclude Special Characters? \n (Y) for yes")
+
+// converts the inputed string data type into a number data type
+  minimum = minimum * 1;
+  max = max * 1;
 
 
+  // Selects which characters would be in the password
   if(lower == 'Y') {
     lower = "abcdefghijklmnopqrstuvwxyz";
-    chars += lower;
   }
   if(upper == 'Y') {
     upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -30,25 +36,22 @@ function generatePassword () {
     chars += special;
   }
 
-  if (lower != 'Y' && upper != 'Y' && num != 'Y' && special != 'Y') {
-    chars = "abcdefghijklmnopqrstuvwxyz"
+
+// random password length between the inputs given;
+  let passwordLength = Math.floor(Math.random() * (max - minimum)) + minimum;
+
+
+// random password
+  for (let i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    newpassword += chars.substring(randomNumber, randomNumber +1)
+
   }
-
-
-  let newpassword;
-
+// directly inserts the password into the page
+  document.getElementById("password").value = newpassword;
 }
 
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
