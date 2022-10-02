00c0e4e782c1f0760d5d7fce87e3cc39f0b9f492
Jose Plasencia <josejpd3@gmail.com>
2022-10-02 11:53:15 -0400

    added prompts and prompt logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword () {
+  let chars = ""
+
+  let minimum = window.prompt("Minimum Password Length: ")
+  let max = window.prompt("Maximum Password Length: ")
+  let lower = window.prompt("Inlclude Lowercase? \n (Y) or (N)")
+  let upper = window.prompt("Inlclude Uppercase? \n (Y) or (N)")
+  let num = window.prompt("Inlclude Numbers? \n (Y) or (N)")
+  let special = window.prompt("Inlclude Special Characters? \n (Y) or (N)")
+
+
+  if(lower == 'Y') {
+    lower = "abcdefghijklmnopqrstuvwxyz";
+    chars += lower;
+  }
+  if(upper == 'Y') {
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    chars += upper;
+  }
+  if(num == 'Y') {
+    num = "0123456789";
+    chars += num ;
+  }
+  if(special == 'Y') {
+    special = "!@#$%^&*()";
+    chars += special;
+  }
+
+  if (lower != 'Y' && upper != 'Y' && num != 'Y' && special != 'Y') {
+    chars = "abcdefghijklmnopqrstuvwxyz"
+  }
+
+
+  let newpassword;
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +51,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
